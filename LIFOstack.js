27b43dee41c5b59193dae7930d15fe92d47d307e
Mike <mkejrvs@gmail.com>
2021-04-12 14:27:52 -0500

    added last in first out stack queue

---
+++ LIFOstack.js
@@ -0,0 +1,44 @@
+class Stack {
+    constructor(head) {
+      this.head = null;
+    }
+
+  add(thing) {
+      let node = new Node(thing);
+
+      if (this.head === null) {
+          this.head = node;
+      } else {
+          node.next = this.head;
+          this.head = node;
+      }
+    }
+
+  remove() {
+    if (this.head ===null) {
+        return undefined;
+    } else {
+        this.head = this.head.next;
+    }
+  }
+}
+class Node {
+    constructor(thing) {
+        this.name = thing;
+        this.next = undefined;
+    }
+}
+
+let queue = new Stack();
+
+/* uncomment 2 test
+
+queue.add('bananas');
+queue.add('mangoes');
+queue.add('pears');
+
+queue.remove();
+
+console.log(queue.head.next);
+
+*/
